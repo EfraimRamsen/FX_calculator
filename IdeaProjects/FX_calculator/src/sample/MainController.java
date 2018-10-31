ab61d5a7739b23ba78aee9a642699f2a80729fed
EfraimRamsen <38281350+EfraimRamsen@users.noreply.github.com>
2018-10-31 14:00:22 +0100

    Initial commit. Most of UI complete, working on MainController.

---
+++ IdeaProjects/FX_calculator/src/sample/MainController.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainController {
+
+	@FXML
+	private Label result;
+	private long number1 = 0;
+	private String operator = "";
+	private boolean start = true;
+
+	@FXML
+	public void processNumbers(ActionEvent event){
+
+	}
+
+	@FXML
+	public void processOperators(ActionEvent event){
+
+	}
+}
