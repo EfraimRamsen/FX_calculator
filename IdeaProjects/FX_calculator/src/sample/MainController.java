3ac565c264751c3bf8c98352674bba1220dba5d6
EfraimRamsen <38281350+EfraimRamsen@users.noreply.github.com>
2018-10-31 14:52:29 +0100

    Basic functions finished

--- IdeaProjects/FX_calculator/src/sample/MainController.java
+++ IdeaProjects/FX_calculator/src/sample/MainController.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class MainController {
@@ -11,14 +12,37 @@ public class MainController {
 	private long number1 = 0;
 	private String operator = "";
 	private boolean start = true;
+	private Model model = new Model();
 
 	@FXML
 	public void processNumbers(ActionEvent event){
-
+		if(start){
+			result.setText("");
+			start = false;
+		}
+		String value = ((Button)event.getSource()).getText(); // stores the button text (number) in variable 'value'
+		result.setText(result.getText() + value);
 	}
 
 	@FXML
 	public void processOperators(ActionEvent event){
+		String value = ((Button)event.getSource()).getText();
 
+		if(!value.equals("=")) {
+			if (!operator.isEmpty()) // checking if operator is not empty
+				return;
+
+			operator = value;
+			number1 = Long.parseLong(result.getText());
+			result.setText("");
+		} else {
+			if(operator.isEmpty())
+				return;
+			long number2 = Long.parseLong(result.getText());
+			float output = model.calculate(number1,number2,operator);
+			result.setText(String.valueOf(output));
+			operator = "";
+			start = true;
+		}
 	}
 }
